2a2a29c28b57fb475306cc9bda47ced66ad40f4b
pauloEzequiel <39318524+pauloEzequiel@users.noreply.github.com>
2018-08-28 20:08:35 -0300

    Add files via upload
    Função que prepara função para transformação!!

---
+++ programacaoLinear/src/programacaoLinear/Simplex.java
@@ -0,0 +1,49 @@
+package programacaoLinear;
+
+public class Simplex {
+
+public static void main(String[] args) {
+		
+		double[][] Restrica={{0.5,0,1,8},{0,1,1,8},{1,2,1,24}};
+		double[]func={40,30};
+		try{
+		montarMatriz(2, 3, Restrica, func);
+		}catch(ArrayIndexOutOfBoundsException e){
+			System.out.println("POR FAVOR, VERIFICAR OS VALORES INFORMADOS,EXISTE INCONSISTÊNCIA NOS ITENS INFORMADOS!!!");	
+		}
+	}
+	
+	
+	public static double[][] montarMatriz(int NumVarDecisao,int numRestricao, double[][]MatrizRestricao,double[]FObjetiva)throws ArrayIndexOutOfBoundsException{
+		int linha=numRestricao+1;
+		int coluna=NumVarDecisao+numRestricao+1;
+		double[][] tabela=new double[linha][coluna];
+		
+		//Preencher tabela com Zero
+		for(int i=0;i<linha;i++){
+			for(int j=0;j<coluna;j++) tabela[i][j]=0;
+		}
+		
+		//Preencher última linha com valores da função objetiva 
+		
+		for(int i=0;i<NumVarDecisao;i++) tabela[numRestricao][i]=FObjetiva[i];
+			
+		
+		for(int i=0;i<numRestricao;i++){
+			tabela[i][coluna-1]=MatrizRestricao[i][NumVarDecisao+1];  //preencher ultima coluna com os valores da última coluna da MatrizRestrição
+			for(int j=0;j<coluna-1;j++) {
+				if(j==NumVarDecisao) tabela[i][j+i]=MatrizRestricao[i][j]; // Insere os coeficientes das variaveis de folgas
+				else if(j<numRestricao)tabela[i][j]=MatrizRestricao[i][j]; // Insere os coeficientes das variaveis de decisão
+			}
+		}
+		
+		for(int i=0;i<linha;i++){
+			for(int j=0;j<coluna;j++){ System.out.print(tabela[i][j]+" ");}
+			System.out.println();
+		}
+		
+		return tabela;
+	}
+
+}
+
